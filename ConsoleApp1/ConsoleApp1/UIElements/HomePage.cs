e18c5798a973c6e09a0af4d91c47be965ddba2ca
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-08-19 14:00:52 -0400

    Beginning of POM

---
+++ ConsoleApp1/ConsoleApp1/UIElements/HomePage.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTestFramework
+{
+    public class HomePage
+    {
+        public HomePage()
+        {
+            PageFactory.InitElements(Driver.driver, this);
+        }
+
+        [FindsBy(How = How.CssSelector, Using = "#page-17 > header > h1")]
+        public IWebElement Headline { get; set; }
+    }
+}
