f0db42d1c061f28896ed4e874e782f9c6e5fe8c9
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-08-19 23:07:56 -0400

    Taking a screenshot with Selenium

---
+++ SeleniumScreenshot/SeleniumScreenshot/EntryPoint.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+
+namespace SeleniumScreenshot
+{
+    class EntryPoint
+    {
+        static void Main()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            string screenshotsDirectory = Directory.GetCurrentDirectory() + @"\screenshots";
+
+
+            driver.Navigate().GoToUrl("https://www.google.com");
+
+            Screenshot googleScreenShot = ((ITakesScreenshot)driver).GetScreenshot();
+
+            if (!Directory.Exists(screenshotsDirectory))
+            {
+                Directory.CreateDirectory(Directory.GetCurrentDirectory() + @"\screenshots\");
+            }
+
+            googleScreenShot.SaveAsFile(Directory.GetCurrentDirectory() + @"\screenshots\googlescreenshot.png", ScreenshotImageFormat.Png);
+
+            driver.Quit();
+        }
+    }
+}
